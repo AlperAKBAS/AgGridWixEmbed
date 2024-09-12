08e8012d48000f55d0e4b506077e447f6e37fd21
alperakbas <33149673+AlperAKBAS@users.noreply.github.com>
2024-09-12 11:21:31 +0300

    minor after initial commit

--- embeddings/ag-grid-basic.js
+++ embeddings/ag-grid-basic.js
@@ -1,1 +1,16 @@
-console.log("Embedding JS file connected successfully...");
+document.addEventListener("DOMContentLoaded", () => {
+  window.onmessage = (event) => {
+    clog(event);
+    clog(event.target);
+    clog(event.data);
+  };
+
+  const msg = "Embedded DOM loaded @" + new Date();
+  clog(msg);
+  window.parent.postMessage({ msg }, "*");
+});
+
+// HELPER FUNCTIONS
+function clog(str) {
+  console.log(`From Embedded: ${str}`);
+}
